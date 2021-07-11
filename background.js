44ceb4d7b10cad59b935645d2624eb211d2767ea
cosmos717 <76693249+cosmos717@users.noreply.github.com>
2021-07-12 05:17:12 +0900

    Delete background.js

--- background.js
+++
@@ -1,9 +0,0 @@
-const body = document.querySelector("body");
-const images = [0,1,2,3,4,5,6,7,8];
-
-const choice = images[Math.floor(Math.random()*images.length)];
-const bgImg = document.createElement("img");
-bgImg.src = `img/${choice}.jpg`;
-bgImg.classList.add("bgImg");
-//document.body.appendChild(bgImg);
-body.style.backgroundImage = `url(${bgImg.src})`;
